1b2aaaaa72a3f036c9dc6564fa60767595db9dbd
Karina <karina.skorka@gmail.com>
2018-02-19 12:52:23 +0100

    Init commit - first project.

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using ReactorSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static DelegateDemo.Program;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow from DelegateDemo project!");
+
+            var wSystem = new ReactorSensorsSystem();
+            var isWeekend = DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday;
+            while(true)
+            {
+                Thread.Sleep(1000);
+                int temp = ReactorSensorsSystem.GetTempFromSensor();
+                if (isWeekend)
+                {
+                    wSystem.CheckReactorTemperature(temp, (new Program()).SendWarningSMS);
+                }
+                else
+                {
+                    wSystem.CheckReactorTemperature(temp, SendWarningEmail);
+                }
+            }
+        }
+
+        public static void SendWarningEmail(string message, int currentTemp)
+        {
+            string emailMsg = message + $"Temperate has changed! Now it equals {currentTemp}";
+
+            //Logic for sending email.
+
+            Console.WriteLine("Email sent!");
+        }
+
+        public void SendWarningSMS(string message, int currentTemp)
+        {
+            string emailMsg = message + $"Temperate has changed! Now it equals {currentTemp}";
+
+            //Logic for texting users.
+
+            Console.WriteLine($"SMS sent! The message was: \n {emailMsg}");
+        }
+    }
+}
---
+++ DelegateDemo/ReactorSystem/ReactorSensorsSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactorSystem
+{
+    public class ReactorSensorsSystem
+    {
+        public delegate void NuclearReactorWarningHandler(string message, int currentTemp);
+
+        public static int GetTempFromSensor()
+        {
+            Random random = new Random();
+            return random.Next(1_000);
+        }
+
+        public void CheckReactorTemperature(int temp, NuclearReactorWarningHandler dlg)
+        {
+            if (temp > 100)
+            {
+                ApplyCoolingPolicy();
+                dlg("Ractor is getting too hot!", temp);
+            }
+            else
+            {
+                Console.WriteLine("No worry! Temperature is still OK!");
+            }
+            //Do other operations.
+        }
+
+        private void ApplyCoolingPolicy()
+        {
+            //Some important stuff ;).
+        }
+    }
+}
